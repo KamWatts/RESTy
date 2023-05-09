3853e5f1297c65e6060290ee6ea58506a61be481
Kameron Watts <kam.watts@gmail.com>
2023-05-09 14:33:26 -0700

    mock API request working

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import './App.scss';
+
+// Let's talk about using index.js and some other name in the component folder.
+// There's pros and cons for each way of doing this...
+// OFFICIALLY, we have chosen to use the Airbnb style guide naming convention. 
+// Why is this source of truth beneficial when spread across a global organization?
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
-import Footer from './Components/Footer';
 
 function App() {
-  
-  const [data, setData] = useState(null);
-  const [requestParams, setRequestParams] = useState({});
-  const callApi = (params) => {
-    // mock output
-    const data = {
-      count: 2,
-      results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-      ],
-    };
-    setData(data);
-    setRequestParams(params);
-  
+
+  const [ request, setRequest ] = useState();
+  const [ response, setResponse ] = useState(null)
+  const [ data, setData ] = useState(null);
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/people/1/')
+    .then( response => response.json())
+    .then( data => {
+      setData(data);
+      setLoading(false);
+    })
+    .catch( error => console.error(error));
+  }, []);
+
   return (
     <>
       <Header />
-      <div>Request Method: {requestParams.method}</div>
-      <div>URL: {requestParams.url}</div>
-      <Form handleApiCall={callApi} />
-      <Results data={data} />
-      <Footer />
+        <div>Request Method: {request}</div>
+        <div>
+          URL: {loading ? <p>Loading...</p> : <Results data={data}/>}
+        </div>
+        <Form request={request} setRequest={setRequest} setResponse={setResponse} />
+        <Results data={response} />
+        <Footer />
     </>
-  );
-  }
+  )
 }
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: null,
+//       requestParams: {},
+//     };
+//   }
+
+//   callApi = (requestParams) => {
+//     // mock output
+//     const data = {
+//       count: 2,
+//       results: [
+//         {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+//         {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+//       ],
+//     };
+//     this.setState({data, requestParams});
+//   }
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Header />
+//         <div>Request Method: {this.state.requestParams.method}</div>
+//         <div>URL: {this.state.requestParams.url}</div>
+//         <Form handleApiCall={this.callApi} />
+//         <Results data={this.state.data} />
+//         <Footer />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
 
 export default App;
--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react';
 import './Form.scss';
 
+
 function Form({ handleApiCall }) {
   const [url, setUrl] = useState('');
+  const [method, setMethod] = useState('GET');
+  const [body, setBody] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
-      method: 'GET',
+      method: method,
       url: url,
+      body: body
     };
     handleApiCall(formData);
   };
+
+  const handleMethodChange = (e) => {
+    setMethod(e.target.id.toUpperCase());
+  }
 
   return (
     <>
@@ -22,11 +30,16 @@ function Form({ handleApiCall }) {
           <button type='submit'>GO!</button>
         </label>
         <label className='methods'>
-          <span id='get'>GET</span>
-          <span id='post'>POST</span>
-          <span id='put'>PUT</span>
-          <span id='delete'>DELETE</span>
+          <span id='get' className={ method === 'GET' ? 'active' : ''} onClick={handleMethodChange}>GET</span>
+          <span id='post' className ={ method === 'POST' ? 'active' : ''} onClick={handleMethodChange}>POST</span>
+          <span id='put' className={ method === 'PUT' ? 'active' : ''} onClick={handleMethodChange}>PUT</span>
+          <span id='delete' className={ method === 'DELETE' ? 'active' : ''} onClick={handleMethodChange}>DELETE</span>
         </label>
+        {(method === 'POST' || method === 'PUT')  &&
+        <label>
+          <span> Body: </span>
+          <textarea name='body' value={body} onChange={ (e) => setBody(e.target.value)} />
+        </label>}
       </form>
     </>
   );
