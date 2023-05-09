f23aee2816ae1330c16b1e86f662b3b6990ec585
Kameron Watts <kam.watts@gmail.com>
2023-05-09 10:02:31 -0700

    utilized hooks in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
-import React from 'react';
-
-import './App.scss';
-
-// Let's talk about using index.js and some other name in the component folder.
-// There's pros and cons for each way of doing this...
-// OFFICIALLY, we have chosen to use the Airbnb style guide naming convention. 
-// Why is this source of truth beneficial when spread across a global organization?
+import React, { useState } from 'react';
 import Header from './Components/Header';
-import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
+import Footer from './Components/Footer';
 
 function App() {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
-  }
-
-  callApi = (requestParams) => {
+  
+  const [data, setData] = useState(null);
+  const [requestParams, setRequestParams] = useState({});
+  
+  const callApi = (params) => {
     // mock output
     const data = {
       count: 2,
@@ -30,21 +18,20 @@ function App() {
         {name: 'fake thing 2', url: 'http://fakethings.com/2'},
       ],
     };
-    this.setState({data, requestParams});
+    setData(data);
+    setRequestParams(params);
   }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div>Request Method: {this.state.requestParams.method}</div>
-        <div>URL: {this.state.requestParams.url}</div>
-        <Form handleApiCall={this.callApi} />
-        <Results data={this.state.data} />
-        <Footer />
-      </React.Fragment>
-    );
-  }
+  
+  return (
+    <>
+      <Header />
+      <div>Request Method: {requestParams.method}</div>
+      <div>URL: {requestParams.url}</div>
+      <Form handleApiCall={callApi} />
+      <Results data={data} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,36 +1,35 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './Form.scss';
 
-function Form() {
+function Form({ handleApiCall }) {
+  const [url, setUrl] = useState('');
 
-  return (
-
-  handleSubmit = (e)  => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
-      method:'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      method: 'GET',
+      url: url,
     };
-    this.props.handleApiCall(formData);
-  },
-  <>
-        <form onSubmit={this.handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-    </>
-    );
+    handleApiCall(formData);
+  };
 
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>URL: </span>
+          <input name='url' type='text' value={url} onChange={(e) => setUrl(e.target.value)} />
+          <button type='submit'>GO!</button>
+        </label>
+        <label className='methods'>
+          <span id='get'>GET</span>
+          <span id='post'>POST</span>
+          <span id='put'>PUT</span>
+          <span id='delete'>DELETE</span>
+        </label>
+      </form>
+    </>
+  );
 }
 
 export default Form;
