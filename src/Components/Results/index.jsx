fbcee9c172784d9e6686b8b9ec4e625b272bea3f
Kameron Watts <kam.watts@gmail.com>
2023-05-10 16:00:47 -0700

    live API request functional

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import JsonView from 'react18-json-view';
+import 'react18-json-view/src/style.css';
+import './Results.scss';
 
 function Results(props) {
+  console.log(props);
 
-    return (
-      <section>
-        <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : 'Loading...'}</pre>
-      </section>
-    );
+  return (
+    <section>
+      {props.data ? (
+        <textarea className="json-textarea" value={JSON.stringify(props.data, null, 2)}>
+          <JsonView src={props.data} stringify={true} />
+        </textarea>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </section>
+  );
 }
 
 export default Results;
